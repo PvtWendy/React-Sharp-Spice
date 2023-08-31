0a6d6d07a91a390360b648f0192c9e0eae8319ba
PvtWendy <102826495+PvtWendy@users.noreply.github.com>
2023-08-31 18:26:09 -0400

    Late first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Article from "./components/Article";
+import Carousel from "./components/Carousel";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Kitchen from "./images/Kitchen.png";
+import Crostini from "./images/Crostini.png";
+import Nutrition from "./images/Nutrition.png";
+import { posts } from "./posts";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Carousel />
+      <section className="articleContainer" id="postsContainer">
+        <Article image={Kitchen} position={"left"} text={posts[0].shortPost} />
+        <Article image=
+      </section>
+      <Footer />
     </div>
   );
 }
