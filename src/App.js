0d024438325220c1896bfc48896843d75b1a3994
PvtWendy <102826495+PvtWendy@users.noreply.github.com>
2023-09-01 00:16:39 -0400

    Made Article component

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import Article from "./components/Article";
 import Carousel from "./components/Carousel";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Kitchen from "./images/Kitchen.png";
 import Crostini from "./images/Crostini.png";
+import Kitchen from "./images/Kitchen.png";
 import Nutrition from "./images/Nutrition.png";
+import Presentation from "./images/Presentation.png";
+import Airfryer from "./images/Airfryer.png";
 import { posts } from "./posts";
 function App() {
   return (
@@ -13,8 +15,11 @@ function App() {
       <Header />
       <Carousel />
       <section className="articleContainer" id="postsContainer">
-        <Article image={Kitchen} position={"left"} text={posts[0].shortPost} />
-        <Article image={}/>
+        <Article image={Kitchen} state={"left"} text={posts[0].shortPost} full={posts[0].longPost}/>
+        <Article image={Crostini} state={"right"} text={posts[1].shortPost} full={posts[1].longPost}/>
+        <Article image={Nutrition} state={"left"} text={posts[2].shortPost}  full={posts[2].longPost}/>
+        <Article image={Presentation} state={"right"} text={posts[3].shortPost} full={posts[3].longPost}/>
+        <Article image={Airfryer} state={"left"} text={posts[4].shortPost} full={posts[4].longPost}/>
       </section>
       <Footer />
     </div>
--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -1,22 +1,35 @@
+import { useState } from "react";
+import { posts } from "../../posts";
+import "./style.css"
+
+import Crostini from "../../images/Crostini.png";
+import Kitchen from "../../images/Kitchen.png";
+import Nutrition from "../..//images/Nutrition.png";
+import Presentation from "../../images/Presentation.png";
+import Airfryer from "../../images/Airfryer.png";
 export default function Article(props) {
+  const [open, setOpen] = useState(false);
+  if (props.state === "left" && open == false) {
     return (
-        props.position === "left" ?
-            <article id={props.key}>
-                <img src={props.image} />
-                <div>
-                    {props.text}
-                    <button>Read More</button>
-                </div>
-
-            </article>
-            :
-            <article id={props.key}>
-
-                <div>
-                    {props.text}
-                    <button>Read More</button>
-                </div>
-                <img src={props.image} />
-            </article>
-    )
+      <article id={props.key}>
+        <img src={props.image} />
+        <div>
+          {props.text}
+          <button onClick={() => setOpen(true)}>Read More</button>
+        </div>
+      </article>
+    );
+  } else if (props.state === "right" && open == false) {
+    return (
+      <article id={props.key}>
+        <div>
+          {props.text}
+          <button onClick={() => setOpen(true)}>Read More</button>
+        </div>
+        <img src={props.image} />
+      </article>
+    );
+  } else {
+    return(props.full)
+  }
 }
